46943eb370c115253f9146480357d4c5c096c8ff
Haifeng Jin <haifeng-jin@users.noreply.github.com>
2021-10-08 20:06:10 -0700

    update

---
+++ docs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+mkdocs build
+git checkout -b gh-pages-temp
+git add -f site
+git commit -m "gh-pages update"
+git subtree split --prefix site -b gh-pages
+git push -f origin gh-pages:gh-pages
+git branch -D gh-pages
+git checkout main
+git branch -D gh-pages-temp
